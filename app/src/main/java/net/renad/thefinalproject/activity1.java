c273657c13db7931fa2a847cdf2edbe6f3c8c7b7
renadmajd <sho3a3.net@gmail.com>
2016-04-06 21:09:53 +0300

    The Final Project
    How To Play Monoploy Game

---
+++ app/src/main/java/net/renad/thefinalproject/activity1.java
@@ -0,0 +1,23 @@
+package net.renad.thefinalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity1);
+
+    }
+
+    public void back2(View view) {
+        this.finish();
+    }
+}
---
+++ app/src/main/java/net/renad/thefinalproject/activity2.java
@@ -0,0 +1,24 @@
+package net.renad.thefinalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.jar.Manifest;
+
+public class activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2);
+
+    }
+
+    public void back(View view) {
+        this.finish();
+}}
